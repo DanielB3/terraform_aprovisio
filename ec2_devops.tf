7d8371821d2b2494c1432d68c0db98ee632d6bdf
Daniel Felipe Barrera Segura <70710398+DanielB3@users.noreply.github.com>
2025-02-24 02:55:40 -0500

    Update ec2_devops.tf

--- ec2_devops.tf
+++ ec2_devops.tf
@@ -1,10 +1,11 @@
 resource "aws_instance" "ec2_devops_finkargo" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.public_subnet_finkargo.id
-  security_groups = [aws_security_group.sg_devops_finkargo.name]
+  ami           = "ami-0885b1f6bd170450c"  # Nueva AMI válida
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet_finkargo_1.id
+  vpc_security_group_ids = [aws_security_group.sg_devops_finkargo.id]
 
   tags = {
     Name = "Finkargo-EC2-DevOps"
   }
 }
+
